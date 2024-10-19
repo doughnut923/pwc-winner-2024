4f76d9029c3b059628a6f01fc7a72a61d2446215
Jonas 🍩 <jonas232005@gmail.com>
2024-10-19 21:56:04 +0800

    stored Login styles to a file

--- HackADay/front-end/src/Register.js
+++ HackADay/front-end/src/Register.js
@@ -1,68 +1,12 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, Typography, Box, Card, CardContent } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Typography, Box, CardContent } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import logo from "./logo.svg"; // Adjust the path to your logo image
-const StyledContainer = styled(Container)(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh',
-    minWidth: '100vw',
-    backgroundColor: theme.palette.background.default,
-}));
 
-const StyledCard = styled(Card)(({ theme }) => ({
-    borderRadius: 40,
-    padding: theme.spacing(3),
-    minWidth: '240px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-}));
-
-const FormContainer = styled(Box)(({ theme }) => ({
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-}));
-
-const StyledForm = styled(Box)(({ theme }) => ({
-    marginTop: theme.spacing(1),
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-}));
-
-const StyledButton = styled(Button)(({ theme }) => ({
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(2),
-    padding: "5px 10px",
-    borderRadius: 20, // Increase border radius
-    width: 'fit-content', // Width to fit content
-
-}));
-
-const StyledTextField = styled(TextField)(({ theme }) => ({
-    width: '100%', // Make the text field span the entire container
-    '& .MuiOutlinedInput-root': {
-        borderRadius: 20, // Set border radius
-        fontSize: '12px', // Set text size to 12px
-        padding: '10px', // Ensure padding is consistent
-    },
-    '& .MuiTextField-root': {
-        marginTop: theme.spacing(1),
-    },
-    '& .MuiInputLabel-root': {
-        fontSize: '12px', // Set label text size to 12px
-    },
-    '& .MuiInputBase-input': {
-        padding: '5px', // Set padding to ensure text alignment
-    },
-    marginBottom: theme.spacing(1), // Reduce the margin-bottom for the first TextField
-}));
+import { StyledContainer, StyledCard, FormContainer, StyledForm, StyledTextField, StyledButton } from "./LoginStyledElements"
 
 const Register = () => {
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repassword, setRePassword] = useState('');
