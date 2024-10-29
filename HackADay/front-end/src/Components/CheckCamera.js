00cf4280a19b88722dd8dc10a672096d86bbcecd
Verizent <101114977+Verizent@users.noreply.github.com>
2024-10-29 16:44:33 +0800

    Update CheckCamera.js

--- HackADay/front-end/src/Components/CheckCamera.js
+++ HackADay/front-end/src/Components/CheckCamera.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import ShowWebcam from './ShowWebcam'; // Adjust the import path as necessary
 
-const CheckCamera = ({handleBlob}) => {
-
+const CheckCamera = ({ handleBlob }) => {
     return (
         <Box
             sx={{
@@ -18,15 +17,26 @@ const CheckCamera = ({handleBlob}) => {
                 height: '60%', // Optional: Set the height
             }}
         >
-            <img 
-                src="https://www.svgrepo.com/show/325179/face-id.svg" 
-                alt="Face ID" 
-                style={{ width: '40px', marginBottom: '0px' }} 
+            <img
+                src="https://www.svgrepo.com/show/325179/face-id.svg"
+                alt="Face ID"
+                style={{ width: '40px', marginBottom: '10px' }} // Adjust marginBottom for spacing
             />
             <Typography variant="h5" component="h1" gutterBottom>
                 Setup Your Camera
             </Typography>
-            <ShowWebcam message={"done"} myWidth={"100"}handleBlob={handleBlob} />
+            <Typography variant="body1" component="p" sx={{ margin: '0 auto', maxWidth: '400px' }}>
+                Please setup your camera correctly! <br />Any suspicious activity will be reported to the exam proctor and may result in your suspension.
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center', // Center the webcam component
+                    marginTop: '20px' // Add some space above the webcam component
+                }}
+            >
+                <ShowWebcam message={"done"} myWidth={"100"} handleBlob={handleBlob} />
+            </Box>
         </Box>
     );
 };
