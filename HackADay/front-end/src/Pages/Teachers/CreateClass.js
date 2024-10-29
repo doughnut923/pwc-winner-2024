5432d41cd6076bdef2133fa68a43f9bcd23bfb84
Jonas 🍩 <62459934+doughnut923@users.noreply.github.com>
2024-10-29 18:22:29 +0800

    updates on UI and readme

--- HackADay/front-end/src/Pages/Teachers/CreateClass.js
+++ HackADay/front-end/src/Pages/Teachers/CreateClass.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography, Box, Grid, IconButton } from '@mui/material';
+import { TextField,Card,  Button, Typography, Box, Grid, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Datetime from 'react-datetime';
 import "react-datetime/css/react-datetime.css";
+import { StyledContainer } from "../ExamOptionStyledElements"
+import { useNavigate } from 'react-router-dom';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useTheme } from '@emotion/react';
 
 const AddClass = () => {
     const [classname, setClassname] = useState('');
     const [startTime, setStartTime] = useState(null);
     const [endTime, setEndTime] = useState(null);
     const [questions, setQuestions] = useState([]);
+
+    const navigate = useNavigate()
 
     const addClassToServer = async () => {
 
@@ -65,79 +71,89 @@ const AddClass = () => {
         setQuestions(newQuestions);
     };
 
+    const theme = useTheme();
+
     return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Add Class
-            </Typography>
-            <Box component="form" noValidate autoComplete="off">
-                <TextField
-                    label="Classname"
-                    value={classname}
-                    onChange={(e) => setClassname(e.target.value)}
-                    fullWidth
-                    margin="normal"
-                />
-                <Box margin="normal">
-                    <Typography variant="body1">Start Time</Typography>
-                    <Datetime
-                        value={startTime}
-                        onChange={(newValue) => setStartTime(newValue)}
-                        inputProps={{ placeholder: 'Select Start Time' }}
-                    />
-                </Box>
-                <Box margin="normal">
-                    <Typography variant="body1">End Time</Typography>
-                    <Datetime
-                        value={endTime}
-                        onChange={(newValue) => setEndTime(newValue)}
-                        inputProps={{ placeholder: 'Select End Time' }}
-                    />
-                </Box>
-                <Typography variant="h6" gutterBottom>
-                    Questions
+        <StyledContainer maxWidth="sm" sx={{
+            overflowY: 'hidden',
+            height: '100vh',
+        }}>
+
+            <Card sx={{ marginTop: "50px", backgroundColor: "#EBEDDF", padding: "2rem", borderRadius: "30px", width:"80%", backgroundColor:"white", height:"80vh", overflowY:"scroll"}}>
+                {/* add a back button to return to /teacher-exam-option */}
+                <Button onClick={() => navigate('/teacher-exam-option')} startIcon={<ArrowBackIosNewIcon />}>Go Back</Button>
+                <Typography variant="h4" gutterBottom >
+                    Add Class
                 </Typography>
-                {questions.map((question, qIndex) => (
-                    <Box key={qIndex} mb={2}>
-                        <Grid container alignItems="center" spacing={2}>
-                            <Grid item xs={11}>
-                                <TextField
-                                    label={`Question ${qIndex + 1} Content`}
-                                    value={question.content}
-                                    onChange={(e) => handleQuestionChange(qIndex, 'content', e.target.value)}
-                                    fullWidth
-                                    margin="normal"
-                                />
-                            </Grid>
-                            <Grid item xs={1}>
-                                <IconButton onClick={() => handleRemoveQuestion(qIndex)}>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </Grid>
-                        </Grid>
-                        <Grid container spacing={2}>
-                            {question.options.map((option, oIndex) => (
-                                <Grid item xs={3} key={oIndex}>
+                <Box component="form" noValidate autoComplete="off">
+                    <TextField
+                        label="Classname"
+                        value={classname}
+                        onChange={(e) => setClassname(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <Box margin="normal">
+                        <Typography variant="h5">Start Time</Typography>
+                        <Datetime
+                            value={startTime}
+                            onChange={(newValue) => setStartTime(newValue)}
+                            inputProps={{ placeholder: 'Select Start Time' }}
+                        />
+                    </Box>
+                    <Box margin="normal">
+                        <Typography variant="h5">End Time</Typography>
+                        <Datetime
+                            value={endTime}
+                            onChange={(newValue) => setEndTime(newValue)}
+                            inputProps={{ placeholder: 'Select End Time' }}
+                        />
+                    </Box>
+                    <Typography variant="h6" gutterBottom>
+                        Questions
+                    </Typography>
+                    {questions.map((question, qIndex) => (
+                        <Box key={qIndex} mb={2}>
+                            <Grid container alignItems="center" spacing={2}>
+                                <Grid item xs={11}>
                                     <TextField
-                                        label={`Option ${oIndex + 1}`}
-                                        value={option}
-                                        onChange={(e) => handleOptionChange(qIndex, oIndex, e.target.value)}
+                                        label={`Question ${qIndex + 1} Content`}
+                                        value={question.content}
+                                        onChange={(e) => handleQuestionChange(qIndex, 'content', e.target.value)}
                                         fullWidth
                                         margin="normal"
                                     />
                                 </Grid>
-                            ))}
-                        </Grid>
-                    </Box>
-                ))}
-                <Button variant="contained" color="primary" onClick={handleAddQuestion}>
-                    Add Question
-                </Button>
-                <Button variant="contained" color="secondary" onClick={addClassToServer} style={{ marginTop: '16px' }}>
-                    Submit Class
-                </Button>
-            </Box>
-        </Container>
+                                <Grid item xs={1}>
+                                    <IconButton onClick={() => handleRemoveQuestion(qIndex)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={2}>
+                                {question.options.map((option, oIndex) => (
+                                    <Grid item xs={3} key={oIndex}>
+                                        <TextField
+                                            label={`Option ${oIndex + 1}`}
+                                            value={option}
+                                            onChange={(e) => handleOptionChange(qIndex, oIndex, e.target.value)}
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Box>
+                    ))}
+                    <Button variant="contained" color="primary" onClick={handleAddQuestion}>
+                        Add Question
+                    </Button>
+                    <Button variant="contained" color="secondary" onClick={addClassToServer} style={{ marginLeft: '16px' }}>
+                        Submit Class
+                    </Button>
+                </Box>
+            </Card>
+        </StyledContainer>
     );
 };
 
--- README.md
+++ README.md
@@ -14,9 +14,53 @@ docker-compose up -d
 ```
 You may need to add sudo in front of the command if you haven't set up docker user permission group.
 
-## Adding a Teacher Role
-To access the `assign-class` page correctly, you need to have the teacher authority your registered account.
+
+## Using the Application
+
+### 1. Registering for a Teacher Account
+
+A teacher account should first be created in order to create and assign the class the the student. once your reach `{your_address}/`, you will be greeted with a login page. Click Sign Up and enter the credentials, click Sign up again. Then you will be prompted to provide face ID, make sure you gave access to the application to access your camera, after aligning the camera to your face, Click done. Then a account would be created.
+
+![SignUp](Teacher_signup.png)
+
+### 2. Adding a Teacher Role
+
+This application is still in development. To add teacher authority to a user
 1. Connect to the SQL Database named `examappdb` with `localhost:3306` with the password and username shown in `compose.yaml` using a method of interest(e.g. [sqlyog](https://github.com/webyog/sqlyog-community/wiki/Downloads))
-2. Under the `t_authority` table, change the value of `permission` to teacher in the corresponding user's row
+2. Under the `t_authority` table, change the value of `permission` to `teacher` for the newly create account. Then apply the changes to the database.
 
+### 3. Create student accounts
 
+Create a student account just like creating a teacher account, but without changing the authority group in sql, as new users are defaulted to be students.
+
+![SignUp](Teacher_signup.png)
+
+### 4. Create and assgining classes
+Login with a teacher's account to access the Teacher's main page.
+
+![alt text](image-1.png)
+
+click Create Class to create a exam, enter the classname and exam start time and click submit class, go back to the teacher's main page and you should see a class show up.
+
+![alt text](image-2.png)
+
+Then click assign classes, click the `+` button on the student you would like to aassign class for. Select the class your would like to add and the class permission should be added.  **(KNOWN BUG: Sometimes newly assigned class would not be displayed. Don't assign same class to people multiple times.)**
+
+![alt text](image-3.png)
+
+### 5. Access the class
+
+For students, they can now see the class they are able to take. Click on one of the classes and they should be good to go.
+
+![alt text](image-4.png)
+
+### 6. Taking the exam
+After redirected to the exampage. They are allow to check if their camera works. If the exam has not started yet, you will be greeted with a countdown page. Once the countdown is reached, it automatically refereshes and shows the questions. Once the exam end time is reached, the students are not allowed to access the exam anymore and be redirected to the login page.
+
+![alt text](image-5.png)
+
+### 7. Monitoring the Exam
+
+For the teacher, in the teach's main page, click on the class created. A dashboard could be seen and student cheating could be inspected by clicking on alerts.
+![alt text](image-6.png)
+![alt text](image-7.png)
